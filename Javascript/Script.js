55fc20468560d8783389b5d74191e20ab090c03e
DanielHBooth <d.booth@uqconnect.edu.au>
2018-08-17 14:50:49 +1000

    Added header

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,1 +1,7 @@
 //Javascript
+$(document).ready(function() {
+    $("#search #submit").click(function(event) {
+        console.log("Longitude: " + $("#longitude_text").val() + "\nLatitude: " + $("#latitude_text").val());
+        event.preventDefault();
+    })
+});
