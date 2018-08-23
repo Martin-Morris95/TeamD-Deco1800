0fb8bf8442ee9e52d34fb89bf0263fa8b826c143
DanielHBooth <d.booth@uqconnect.edu.au>
2018-08-23 21:28:37 +1000

    Moved intialization of google maps to the callback function initMap instead of the page loaded function - occasionally the callback function was called before the page had finished loading which meant google maps did not initialize properly. Also made the map change position when user entered coordinates and added an event for the user clicking on the map

--- Javascript/Maps.js
+++ Javascript/Maps.js
@@ -1,13 +1,39 @@
-function initMap() {} 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), { center: {lat: -34.397, lng: 150.644},
+          zoom: 8});
+  // and other stuff...
+  $("#search #submit").click(function(event) {
+    event.preventDefault();
+    var longitude = $("#longitude_text").val();
+    var latitude = $("#latitude_text").val();
+    map.panTo(new google.maps.LatLng(latitude, longitude));
+    console.log("Longitude: " + $("#longitude_text").val() + "\nLatitude: " + $("#latitude_text").val());
+  });
 
+  map.addListener('click', function(event) {
+    var longitude = event.latLng.lng();
+    var latitude = event.latLng.lat();
+    console.log("Longitude: " + longitude + "\nLatitude" + latitude);
+  });
+} 
+
+/*
 $(() => {
   initMap = function() {
     // your code like...
     var map = new google.maps.Map(document.getElementById('map'), { center: {lat: -34.397, lng: 150.644},
           zoom: 8});
     // and other stuff...
+    $("#search #submit").click(function(event) {
+      event.preventDefault();
+      var longitude = $("#longitude_text").val();
+      var latitude = $("#latitude_text").val();
+      map.panTo(new google.maps.LatLng(latitude, longitude));
+      console.log("Longitude: " + $("#longitude_text").val() + "\nLatitude: " + $("#latitude_text").val());
+    });
   }
 });
+*/
 
 function updateMap(latt, long ){
     map.longitude = long;
--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,7 +1,9 @@
 //Javascript
+/*
 $(document).ready(function() {
     $("#search #submit").click(function(event) {
         event.preventDefault();
         console.log("Longitude: " + $("#longitude_text").val() + "\nLatitude: " + $("#latitude_text").val());
     })
 });
+*/
